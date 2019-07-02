92a7e500dac95a56916a297b75cc8516be974c26
luanyanxu <1061639516@qq.com>
2019-07-02 18:48:37 +0800

    add coding2 for offer

---
+++ src/coding2/Hello.java
@@ -0,0 +1,12 @@
+package coding2;
+
+/**
+ * @Author: luanyanxu
+ * @Date: 2019/7/2 18:47
+ * @Version 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
