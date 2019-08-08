44515479b2133d31741cb0a1e421f2a39b4763ac
luanyanxu <1061639516@qq.com>
2019-08-08 18:25:11 +0800

    下一个排列

---
+++ src/coding2/leetcode/Leetcode31.java
@@ -0,0 +1,54 @@
+package coding2.leetcode;
+
+/**
+ * @Author: luanyanxu
+ * @Date: 2019/8/8 17:29
+ * @Version 1.0
+ */
+public class Leetcode31 {
+
+    public void nextPremutation(int []nums){
+
+        int  i = nums.length-2;
+
+        while (i >= 0 && nums[i+1] <= nums[i]){
+            i--;
+        }
+
+        if(i >= 0){
+            int j = nums.length-1;
+            while (j >= 0 && nums[j]<= nums[i]){
+                j--;
+            }
+            swap(nums,i,j);
+        }
+
+        reverse(nums,i+1);
+    }
+
+    private void reverse(int[] nums, int start) {
+
+        int  i =start,j = nums.length-1;
+        while (i < j){
+            swap(nums,i,j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+
+    public static void main(String[] args) {
+        int [] nums = {1,2,3,6,7,5};
+        new Leetcode31().nextPremutation(nums);
+        for (int i = 0; i <=nums.length-1; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+}
