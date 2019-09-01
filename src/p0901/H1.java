2756a849a5fa881f3b57926468f7a9db3efc6581
luanyanxu <1061639516@qq.com>
2019-09-01 15:20:55 +0800

    添加H1类

---
+++ src/p0901/H1.java
@@ -0,0 +1,13 @@
+package p0901;
+
+/**
+ * @Author: luanyanxu
+ * @Date: 2019/9/1 15:19
+ * @Version 1.0
+ */
+public class H1 {
+
+    public static void main(String[] args) {
+        System.out.println("hello yangshuaitong");
+    }
+}
