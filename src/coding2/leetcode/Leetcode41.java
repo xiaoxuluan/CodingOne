4995688d55f466494f961b4877a7bfddd3695725
luanyanxu <1061639516@qq.com>
2019-09-09 16:41:26 +0800

    新增注释

--- src/coding2/leetcode/Leetcode41.java
+++ src/coding2/leetcode/Leetcode41.java
@@ -9,17 +9,23 @@ public class Leetcode41 {
 
     public int firstMissingPositive(int[] nums) {
         int len = nums.length;
+        //定义一个新数组，最小的数字肯定就在新数组的下标中
         int[] arr = new int[len + 2];
         for (int item : nums) {
+            //排除负数和大于输入数组长度的数，因为缺失的正数肯定小于数组的长度+1（这里需要仔细想清楚）
             if (item > 0 && item <= len) {
+                //新数组下标为1，则表示有这个数字
                 arr[item] = 1;
             }
         }
+
+        //遍历新数组 看看第一个没有的正整数是谁就行了
         for (int i = 1; i < len + 2; i++) {
             if (0 == arr[i]) {
                 return i;
             }
         }
+        //这一步为了应对空数组
         return len + 1;
     }
 
