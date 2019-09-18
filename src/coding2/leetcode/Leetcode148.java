8dc463c29f9717d50951ec4b6fa0b58ec4340644
luanyanxu <1061639516@qq.com>
2019-09-18 11:08:50 +0800

    148 排序链表

--- src/coding2/leetcode/Leetcode148.java
+++ src/coding2/leetcode/Leetcode148.java
@@ -6,6 +6,22 @@ package coding2.leetcode;
  * @Version 1.0
  */
 public class Leetcode148 {
+    /**
+     * 在?O(n?log?n) 时间复杂度和常数级空间复杂度下，对链表进行排序。
+     *
+     * 示例 1:
+     *
+     * 输入: 4->2->1->3
+     * 输出: 1->2->3->4
+     * 示例 2:
+     *
+     * 输入: -1->5->3->4->0
+     * 输出: -1->0->3->4->5
+     *
+     * @param head
+     * @return
+     */
+
     public ListNode sortList(ListNode head){
         if(head == null || head.next == null){
             return head;
@@ -32,15 +48,5 @@ public class Leetcode148 {
         }
         h.next = left !=null ? left:right;
         return result.next;
-    }
-
-
-
-}
-class ListNode{
-    int val;
-    ListNode next;
-    ListNode(int x){
-        val = x;
     }
 }
---
+++ src/coding2/leetcode/ListNode.java
@@ -0,0 +1,9 @@
+package coding2.leetcode;
+
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int x){
+        val = x;
+    }
+}
