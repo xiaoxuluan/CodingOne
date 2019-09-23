99c8ecf35cf6217a5bf5c45f45f3794a25bcefd4
luanyanxu <1061639516@qq.com>
2019-09-23 13:41:38 +0800

    206 反转链表

---
+++ src/coding2/leetcode/Leetcode206.java
@@ -0,0 +1,22 @@
+package coding2.leetcode;
+
+/**
+ * @Author: luanyanxu
+ * @Date: 2019/9/23 13:39
+ * @Version 1.0
+ */
+public class Leetcode206 {
+
+    public ListNode reverseListNode(ListNode head){
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null){
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+
+        return pre;
+    }
+}
