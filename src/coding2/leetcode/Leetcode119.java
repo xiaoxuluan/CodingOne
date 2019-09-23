9774c56bada743e8467a60ee86ec1911ed71bea2
luanyanxu <1061639516@qq.com>
2019-09-23 15:00:27 +0800

    119 杨辉三角II

---
+++ src/coding2/leetcode/Leetcode119.java
@@ -0,0 +1,31 @@
+package coding2.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: luanyanxu
+ * @Date: 2019/9/23 14:51
+ * @Version 1.0
+ */
+public class Leetcode119 {
+
+
+
+    public List<Integer> getrow(int rownums){
+        List<Integer> result = new ArrayList<>(rownums+1);
+        long index = 1;
+        for (int i = 0; i <= rownums; i++) {
+            result.add((int) index);
+            index = index * (rownums - i) / (i+1);
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+
+        List<Integer > list = new Leetcode119().getrow(3);
+        System.out.println(list);
+    }
+}
