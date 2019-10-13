84acf7c66421de5b2496a66be42a0a86478ab82c
luanyanxu <1061639516@qq.com>
2019-10-14 01:03:03 +0800

    锁实践

---
+++ src/coding/multiThread/MyLock.java
@@ -0,0 +1,23 @@
+package coding.multiThread;
+
+/**
+ * @Author: luanyanxu
+ * @Date: 2019/10/13 11:59
+ * @Version 1.0
+ */
+public class MyLock {
+
+    public static void main(String[] args) {
+        Integer a = 999;
+        Integer b = 999;
+
+        Integer c = 9;
+        Integer d = 9;
+        System.out.println(a==b);
+        System.out.println(c==d);
+
+        System.out.println(a.equals(b));
+
+
+    }
+}
---
+++ src/coding/threadlocaltest/Bank.java
@@ -0,0 +1,57 @@
+package coding.threadlocaltest;
+
+/**
+ * @Author: luanyanxu
+ * @Date: 2019/10/13 14:58
+ * @Version 1.0
+ */
+ class Bank {
+
+    ThreadLocal<Integer> t = new ThreadLocal<Integer>(){
+        @Override
+        protected Integer initialValue(){
+            return 100;
+        }
+    };
+
+    public int get(){
+        return t.get();
+    }
+
+    public void  set(){
+        t.set(t.get()+10);
+    }
+
+}
+
+class Transfer implements Runnable{
+
+     Bank bank;
+
+     public Transfer(Bank bank){
+         this.bank = bank;
+     }
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            bank.set();
+            System.out.println(Thread.currentThread()+" --" +bank.get());
+        }
+    }
+}
+
+ class De{
+
+     public static void main(String[] args) throws InterruptedException {
+         Bank bank = new Bank();
+         Transfer transfer = new Transfer(bank);
+
+         Thread t1 = new Thread(transfer);
+         t1.start();
+         Thread t2 = new Thread(transfer);
+         t2.start();
+         t1.join();
+         t2.join();
+         System.out.println(bank.get());
+     }
+}
