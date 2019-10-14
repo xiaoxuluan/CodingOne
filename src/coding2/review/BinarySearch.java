48d22d366cc5f58d965e20cb2119004505de8000
luanyanxu <1061639516@qq.com>
2019-10-14 17:54:15 +0800

    二分查找

---
+++ src/coding2/review/BinarySearch.java
@@ -0,0 +1,36 @@
+package coding2.review;
+
+/**
+ * @Author: luanyanxu
+ * @Date: 2019/10/14 17:38
+ * @Version 1.0
+ */
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int [] data = {1,2,3,6,9};
+        int target = 3;
+
+
+
+        int res = 0;
+
+        int low = 0;
+        int high = data.length;
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(target==data[mid]){
+                res= mid;
+            }else if(target<data[mid]){
+                high = mid -1;
+            }else{
+                low = mid+1;
+            }
+        }
+
+        System.out.println(res);
+    }
+
+
+
+}
