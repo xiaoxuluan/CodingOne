8bd4be45a4df41ac16f3e966c4a429e25e07aa33
Luan <1061639516@qq.com>
2019-11-07 11:07:09 +0800

    mac first commit

---
+++ src/coding2/review/MacJavaTest.java
@@ -0,0 +1,11 @@
+package coding2.review;
+
+public class MacJavaTest {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello mac  java programing test");
+
+        System.out.println("git clone pull add push run ");
+    }
+}
