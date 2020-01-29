a9c7ffb146d09bd9cdacd5f1ea9d25e19122ccc8
1061639516@qq.com <luan19940911>
2020-01-29 20:22:13 +0800

    memorysearch

---
+++ src/coding/dp/MemorySearch.java
@@ -0,0 +1,59 @@
+package coding.dp;
+
+import java.util.Scanner;
+
+/**
+ * @author luanyanxu
+ */
+public class MemorySearch {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Scanner scanner = new Scanner(System.in);
+        for(int i=0; i<25; i++) {
+            for(int j=0; j<25; j++) {
+                for(int k=0; k<25; k++) {
+                    ww[i][j][k] = 0;
+                }
+            }
+        }
+        while(true) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int c = scanner.nextInt();
+            if(a == -1 && b == -1 && c == -1) {
+                break;
+            }else {
+                System.out.println("w(" + a + ", " + b + ", " + c + ") = " + w(a, b, c));
+            }
+        }
+        scanner.close();
+    }
+
+    private static int[][][] ww= new int[25][25][25];
+
+    public static int w(long a, long b, long c) {
+
+        if(a<=0 || b<=0 || c<=0) {
+            return 1;
+        }else if(a>20 || b>20 || c>20){
+            return w(20, 20, 20);
+        }else {
+            if(ww[(int) a][(int) b][(int) c] == 0) {
+                if(a < b && b < c)
+                {
+                    ww[(int) a][(int) b][(int) c] =  w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+                }
+                else
+                {
+                    ww[(int) a][(int) b][(int) c] =  w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+                }
+                return ww[(int) a][(int) b][(int) c];
+            }else {
+                return ww[(int) a][(int) b][(int) c];
+            }
+        }
+
+    }
+}
---
+++ src/coding2/leetcode/Leetcode329.java
@@ -0,0 +1,46 @@
+package coding2.leetcode;
+
+/**
+ * @author luanyanxu
+ */
+public class Leetcode329 {
+
+    private static final int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    private int m, n;
+    private int longestIncreasingPath1(int [][] matrix){
+        if(matrix.length == 0) {
+            return 0;
+        }
+        m = matrix.length;
+        n = matrix[0].length;
+        int [] [] cache = new int[m] [n];
+        int ans = 0;
+        for (int i = 0;i < m; ++i){
+            for (int j = 0;j< n;++j){
+                ans = Math.max(ans,dfs(matrix,i,j,cache));
+            }
+        }
+        return ans;
+    }
+
+    private int dfs (int [][] matrix,int i,int j ,int [] [] cache){
+        if(cache[i][j] != 0){
+            return cache[i][j];
+        }
+        for (int [] d: dirs) {
+            int x = i + d[0],y = j+ d[1];
+            if(0 <= x && x< m && 0<=y && y< n && matrix[x][y] > matrix[i][j]){
+                cache[i][j] =Math.max(cache[i][j],dfs(matrix,x,y,cache));
+            }
+        }
+
+        return ++cache[i][j];
+    }
+
+    public static void main(String[] args) {
+        Leetcode329 leetcode329 = new Leetcode329();
+        int result = leetcode329.longestIncreasingPath1(dirs);
+        System.out.println(result);
+    }
+
+}
