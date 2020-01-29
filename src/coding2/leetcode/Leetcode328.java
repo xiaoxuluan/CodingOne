4c4fe9ce78c2fc212d1d0d1d39de30dcebdb0487
1061639516@qq.com <luan19940911>
2020-01-29 20:21:49 +0800

    leetcode328

---
+++ src/coding2/leetcode/Leetcode328.java
@@ -0,0 +1,31 @@
+package coding2.leetcode;
+
+public class Leetcode328 {
+
+    public ListNode oddEventList(ListNode head){
+        if(head == null || head.next == null || head.next.next ==null){
+            return head;
+        }
+
+        ListNode odd = new ListNode(-1);
+        ListNode oddHead = odd;
+        ListNode even  = new ListNode(-1);
+        ListNode evenHead = even;
+        while (head != null && head.next != null){
+            odd.next = head;
+            even.next = head.next;
+            odd = odd.next;
+            even = even.next;
+            head = head.next.next;
+        }
+
+        if(head != null){
+            odd.next = head;
+            odd = odd.next;
+            even.next = null;
+        }
+
+        odd.next = evenHead.next;
+        return oddHead.next;
+    }
+}
