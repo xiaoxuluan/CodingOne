578f976c6c46ab07cc0e158ffb9d80715a7160ec
1061639516@qq.com <luan19940911>
2020-03-03 14:32:10 +0800

    三目运算符

---
+++ src/coding2/review/Sanmuyunsuanfu.java
@@ -0,0 +1,16 @@
+package coding2.review;
+
+class SanMuYunSuanFu {
+    public static void main(String[] args) {
+
+        //三目运算符
+        //问号 ？ 前面是判断条件
+        // 如果条件成立    取冒号 ： 前面的值
+        // 如果条件不成立  取冒号 ： 后面的值
+        int a = 0;
+        a = 10 > 8 ? 10 : 8;
+        System.out.println(a);
+
+
+    }
+}
