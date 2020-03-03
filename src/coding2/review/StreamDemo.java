7733ef41fb3cc3a8a4ec8049acf2c6124afb0d2a
1061639516@qq.com <luan19940911>
2020-03-03 14:31:37 +0800

    去重处理

--- src/coding2/review/StreamDemo.java
+++ src/coding2/review/StreamDemo.java
@@ -4,6 +4,7 @@ import coding.producerconsumer.Consumer;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author luanyanxu
@@ -15,7 +16,15 @@ public class StreamDemo {
     }
 
     public static void main(String[] args) {
-        List<String> al = Arrays.asList("a","b","c","d");
-        al.forEach(StreamDemo::printValue);
+        List<String> al = Arrays.asList("a","b","c","d","a");
+        //al.forEach(StreamDemo::printValue);
+        String al1 =al.stream().distinct().collect(Collectors.joining(","));
+        System.out.println(al1);
+
+        //去重处理
+        List<String> list = Arrays.asList("AA", "BB", "CC", "BB", "CC", "AA", "AA");
+        long l = list.stream().distinct().count();
+        String output = list.stream().distinct().collect(Collectors.joining(","));
+        System.out.println(output);
     }
 }
