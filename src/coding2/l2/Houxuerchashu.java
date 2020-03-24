1a3155b201e8d6f574b0a7e01bef8e826382039b
luanyanxu <1061639516@qq.com>
2020-03-24 23:51:51 +0800

    后序遍历二叉树

---
+++ src/coding2/l2/Houxuerchashu.java
@@ -0,0 +1,40 @@
+package coding2.l2;
+
+import coding2.BinaryTree.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @Author: luanyanxu
+ * @Date: 2020/3/24 0:28
+ * @Version 1.0
+ */
+public class Houxuerchashu {
+
+    //时间复杂度 空间复杂度 O(N)
+    public List<Integer> postOrderTree(TreeNode root){
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        LinkedList<Integer> output = new LinkedList<>();
+
+        if(root == null){
+            return output;
+        }
+
+        stack.add(root);
+        while (!stack.isEmpty()){
+            TreeNode node = stack.pollLast();
+            output.addFirst(node.getData());
+
+            if(node.getLeftChild() != null){
+                stack.add(node.getLeftChild());
+            }
+
+            if (node.getRightChild() != null){
+                stack.add(node.getRightChild());
+            }
+        }
+
+        return output;
+    }
+}
